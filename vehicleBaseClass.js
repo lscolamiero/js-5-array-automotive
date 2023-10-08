5d81f3a7a726a3e1219e940755ea344970a270e6
Lautaro Scolamiero <lautaro.scolamiero@gmail.com>
2023-10-08 20:58:12 +0200

    methods added

--- vehicleBaseClass.js
+++ vehicleBaseClass.js
@@ -1,16 +1,16 @@
 class Vehicle {
     constructor(make, model, year, color, mileage){
-        this.make = make
-        this.model = model
-        this.year = year
-        this.color = color
-        this.gasoline = mileage
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+        this.gasoline = mileage;
     }
         vehicleInfo() {
-          console.log("Make: " + this.make + ", " + "Model: " + this.model + ", " + "Color: " + this.color)
+          console.log("Make: " + this.make + ", " + "Model: " + this.model + ", " + "Color: " + this.color + ", " + "Miles: " + this.mileage)
     }
 }
 
-let car = new Vehicle("Jaguar", "Turbo XE", "Black");
+let car = new Vehicle("Jaguar", "Turbo XE", "Black", "33000");
 car.vehicleInfo();
 
